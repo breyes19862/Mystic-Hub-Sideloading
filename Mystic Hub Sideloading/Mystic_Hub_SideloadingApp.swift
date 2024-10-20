db4e3bd94e2fbeb133ba0f71bd457edf7c19624d
Star Services LLC <breyes19862@att.net>
2024-10-20 14:58:40 -0400

    Initial Commit

---
+++ Mystic Hub Sideloading/Mystic_Hub_SideloadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mystic_Hub_SideloadingApp.swift
+//  Mystic Hub Sideloading
+//
+//  Created by Bryan Reyes on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Mystic_Hub_SideloadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
